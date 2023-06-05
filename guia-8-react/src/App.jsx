b780beb6e9276fcc0e006a6c63835a1ee66b32cf
Daniela Manjarres Virviescas <danielamvirviescas2004@gmail.com>
2023-06-04 21:44:13 -0500

    Creacion y realizacion del primer ejercicio

---
+++ guia-8-react/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import Counter from './componentes/component1/Counter'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Counter/>
+    </>
+  )
+}
+
+export default App
