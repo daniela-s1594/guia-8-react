c20e77efcfb1c489aa188b4ce73ca1a6c96e0ebd
DavidCv0914 <dabeats2004@gmail.com>
2023-06-06 20:49:13 -0500

    Terminacion del ejercicio 3.Dmv

--- guia-8-react/src/App.jsx
+++ guia-8-react/src/App.jsx
@@ -1,12 +1,13 @@
 //import { useState } from 'react'
-import ListaTareas from './componentes/component1/ListaTareas'
+//import ListaTareas from './componentes/component1/ListaTareas'
 //import Counter from './componentes/component1/Counter'
+import DiceGame from './componentes/component1/DiceGame'
 import './App.css'
 
 function App() {
   return (
     <>
-      <ListaTareas/>
+      <DiceGame/>
     </>
   )
 }
